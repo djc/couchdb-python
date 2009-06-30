cfb228ce6722b73c2362dfba2067259440f20d8d
Christopher Lenz <cmlenz@gmx.de>
2009-06-30 08:35:19 +0000

    Update a couple of copyright years.
    --HG--
    extra : convert_revision : svn%3A7a298fb0-333a-0410-83e7-658617cd9cf3/trunk%40163

--- couchdb/view.py
+++ couchdb/view.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2007 Christopher Lenz
+# Copyright (C) 2007-2008 Christopher Lenz
 # All rights reserved.
 #
 # This software is licensed as described in the file COPYING, which
