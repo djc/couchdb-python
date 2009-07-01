561d61290c4d1da1ef2ce275c6d526c705adb032
Christopher Lenz <cmlenz@gmx.de>
2009-07-01 16:52:57 +0000

    Accept (and ignore, for now) extra configuration options supplied with the 'reset' command in the Python view server.
    --HG--
    extra : convert_revision : svn%3A7a298fb0-333a-0410-83e7-658617cd9cf3/trunk%40175

--- couchdb/view.py
+++ couchdb/view.py
@@ -39,7 +39,7 @@ def run(input=sys.stdin, output=sys.stdout):
         output.write('\n')
         output.flush()
 
-    def reset():
+    def reset(config=None):
         del functions[:]
         return True
 
