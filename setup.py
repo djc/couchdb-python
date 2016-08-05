87808bcab899d26ddd1573ba57b8da6a8e379283
Dirkjan Ochtman <dirkjan@ochtman.nl>
2016-08-05 14:41:19 +0200

    Bumping version to 1.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 
 setup(
     name = 'CouchDB',
-    version = '1.0.2',
+    version = '1.1',
     description = 'Python library for working with CouchDB',
     long_description = \
 """This is a Python library for CouchDB. It provides a convenient high level
